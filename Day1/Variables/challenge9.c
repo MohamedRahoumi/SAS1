5a221761de4d48e02738673ef06f17052843bfaa
MohamedRahoumi <mohamedrahoumi4@gmail.com>
2025-09-07 21:18:34 +0100

    Distance 3D entre deux points

---
+++ Day1/Variables/challenge9.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>   
+
+int main() {
+    float x1, y1, z1, x2, y2, z2, distance;
+
+    
+    printf("Entrez les coordonnees (x1 y1 z1) : ");
+    scanf("%f %f %f", &x1, &y1, &z1);
+
+    
+    printf("Entrez les coordonnees (x2 y2 z2) : ");
+    scanf("%f %f %f", &x2, &y2, &z2);
+ 
+    distance = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2) + pow(z2 - z1, 2));
+
+
+    printf("La distance entre les deux points est : %.2lf\n", distance);
+
+    return 0;
+}
