d6c38b2a12e1b855f4dc1b791103843d8de4ec9a
MohamedRahoumi <mohamedrahoumi4@gmail.com>
2025-09-07 21:24:17 +0100

    Challenge 8 : Mention Obtenue

---
+++ Day1/ConditionsLM1/challenge8.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    float moyenne;
+
+ 
+    printf("Entrez la moyenne de l'eleve : ");
+    scanf("%f", &moyenne);
+
+
+    if (moyenne < 10) {
+        printf("Mention : Recale\n");
+    } 
+    else if (moyenne < 12) {   
+        printf("Mention : Passable\n");
+    } 
+    else if (moyenne < 14) { 
+        printf("Mention : Assez bien\n");
+    } 
+    else if (moyenne < 16) {   
+        printf("Mention : Bien\n");
+    } 
+    else {                     
+        printf("Mention : Tres bien\n");
+    }
+
+    return 0;
+}
