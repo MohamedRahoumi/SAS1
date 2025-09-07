141e6362714e033c57b2af388a22c28963bd8431
MohamedRahoumi <mohamedrahoumi4@gmail.com>
2025-09-07 21:24:25 +0100

    Challenge 12 : Comparaison d'Instants

---
+++ Day1/ConditionsLM1/challenge12.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int h1, m1, s1;
+    int h2, m2, s2;
+
+    printf("Entrez le premier instant (HH:MM:SS) : ");
+    scanf("%d:%d:%d", &h1, &m1, &s1);
+
+    printf("Entrez le deuxieme instant (HH:MM:SS) : ");
+    scanf("%d:%d:%d", &h2, &m2, &s2);
+
+    if (h1 < h2 || (h1 == h2 && m1 < m2) || (h1 == h2 && m1 == m2 && s1 < s2)) {
+        printf("Le premier instant vient avant le deuxieme.\n");
+    } 
+    else if (h1 == h2 && m1 == m2 && s1 == s2) {
+        printf("Il s'agit du meme instant.\n");
+    } 
+    else {
+        printf("Le deuxieme instant vient avant le premier.\n");
+    }
+    return 0;
+}
